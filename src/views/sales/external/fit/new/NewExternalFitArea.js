9b19c2e1dddd685cd1871d322190f08096476547
Alex Hodson <alexhodson72@gmail.com>
2020-07-31 10:20:55 +0100

    feat(table header display): add table header display with interaction nodes
    Adds a table header display for the new external supply fit jobs with a search field and refresh
    button
    
    #12

--- src/views/sales/external/fit/new/NewExternalFitArea.js
+++ src/views/sales/external/fit/new/NewExternalFitArea.js
@@ -1,9 +1,16 @@
 import React, { useEffect } from 'react'
+import { FaSearch, FaSyncAlt } from 'react-icons/fa'
+
 import Loader from '../../../../../components/loader/Loader'
 import Table from '../../../../../components/table/Table'
 import useLoader from '../../../../../hooks/loader/useLoader'
 import useNewFitJobRetrieval from '../../../../../hooks/sales/fit/new/useNewFitJobRetrieval'
 import FIT_TYPE from '../../../../../util/constants/FitType'
+
+import '../../../../../style/header.css'
+import '../../../../../style/form-control.css'
+import '../../../../../style/button.css'
+import '../../../../../style/icon.css'
 
 /**
  * @author Alex Hodson : it-alex@windows-plus.co.uk
@@ -33,8 +40,38 @@ const NewExternalFitArea = () => {
 			{ showLoader && <Loader /> }
 			{! showLoader && (
 				<>
-					<h3>New External Supply Fit Jobs</h3>
+					<div className="section-heading bg-blue d-flex justify-content-md-between align-items-center">
+						<>
+							<h3>External Supply Fit Jobs</h3>
+						</>
+						<div className="d-flex justify-content-md-end align-items-center">
+							<div className="form-label-group mx-2">
+								<input
+									type="text"
+									id="searchField"
+									className="form-control"
+									placeholder="Email address"
+									required
+								/>
+								<label htmlFor="searchField">
+									Search
+									<div className="control-icon">
+										<FaSearch />
+									</div>
+								</label>
+							</div>
+							<div className="form-label-group mx-2">
+								<button type="button" className="custom btn btn-sm btn-primary">
+									<span>Refresh</span>
+									<span className="button-icon">
+										<FaSyncAlt />
+									</span>
+								</button>
+							</div>
+						</div>
+					</div>
 					<>
+						<h4>New Jobs</h4>
 						<Table tableHeaders={tableHeaders} tableRows={tableRows} />
 					</>
 				</>
